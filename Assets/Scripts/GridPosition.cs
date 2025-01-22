a77108428957a3382e9d70c996dbaa3639051d87
AhmetGjinofci <gjinofciahmet@gmail.com>
2025-01-22 17:34:41 +0100

    Added a GameManager script to manage game states
    - Applied the Singleton Pattern to ensure only one instance of GameManager exists
    - Included basic setup for game initialization and state management

---
+++ Assets/Scripts/GridPosition.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+
+public class GridPosition : MonoBehaviour
+{
+
+    [SerializeField] private int x;
+    [SerializeField] private int y;
+
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Click! " + x + ", " + y);
+        GameManager.Instance.ClickedOnGridPosition(x, y);
+    }
+
+}
