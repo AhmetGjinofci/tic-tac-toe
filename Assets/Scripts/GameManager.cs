c2c4542d7d8d33d39a8ceefc0687cd4ec6264810
AhmetGjinofci <gjinofciahmet@gmail.com>
2025-01-23 18:18:03 +0100

    Add player types and implement LocalClientID handling and Implement Player UI
    - Integrated LocalClientID to track and manage client-specific data
    - Designed and added UI components specific to player interaction
    - Linked Player UI to gameplay data for real-time updates

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,8 @@
 using System;
+using Unity.Netcode;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : NetworkBehaviour
 {
 
     public static GameManager Instance { get; private set; }
@@ -12,7 +13,24 @@ public class GameManager : MonoBehaviour
     {
         public int x;
         public int y;
+        public PlayerType playerType;
     }
+
+
+    public event EventHandler OnGameStarted;
+    public event EventHandler OnCurrentPlayablePlayerTypeChanged;
+
+
+    public enum PlayerType
+    {
+        None,
+        Cross,
+        Circle,
+    }
+
+
+    private PlayerType localPlayerType;
+    private PlayerType currentPlayablePlayerType;
 
 
 
@@ -27,14 +45,92 @@ public class GameManager : MonoBehaviour
     }
 
 
+    public override void OnNetworkSpawn()
+    {
+        Debug.Log("OnNetworkSpawn: " + NetworkManager.Singleton.LocalClientId);
 
-    public void ClickedOnGridPosition(int x, int y)
+        if(NetworkManager.Singleton.LocalClientId == 0)
+        {
+            localPlayerType = PlayerType.Cross;
+        }
+        else
+        {
+            localPlayerType = PlayerType.Circle;
+        }
+
+        if (IsServer)
+        {
+            NetworkManager.Singleton.OnClientConnectedCallback += NetworkManager_OnClientConnectedCallback;
+        }
+    }
+
+    private void NetworkManager_OnClientConnectedCallback(ulong obj)
+    {
+        if (NetworkManager.Singleton.ConnectedClientsList.Count == 2)
+        {
+            //Start Game
+            currentPlayablePlayerType = PlayerType.Cross;
+            TriggerOnGameStartedRpc();
+        }
+    }
+
+
+    [Rpc(SendTo.ClientsAndHost)]
+    private void TriggerOnGameStartedRpc()
+    {
+        OnGameStarted?.Invoke(this, EventArgs.Empty);
+    }
+
+
+    [Rpc(SendTo.Server)]
+    public void ClickedOnGridPositionRpc(int x, int y, PlayerType playerType)
     {
         Debug.Log("ClickedOnGridPosition " + x + ", " + y);
+
+        if(playerType != currentPlayablePlayerType)
+        {
+            return;
+        }
+
         OnClickedOnGridPosition?.Invoke(this, new OnClickedOnGridPositionEventArgs{
             x = x,
             y = y,
+            playerType = playerType,
         });
+
+        switch (currentPlayablePlayerType)
+        {
+            default:
+            case PlayerType.Cross:
+                currentPlayablePlayerType = PlayerType.Circle;
+                break;
+            case PlayerType.Circle:
+                currentPlayablePlayerType = PlayerType.Cross;
+                break;
+        }
+
+        TriggerOnCurrentPlayablePlayerTypeChanged();
+
+    }
+
+
+    [Rpc(SendTo.ClientsAndHost)]
+    private void TriggerOnCurrentPlayablePlayerTypeChanged()
+    {
+        OnCurrentPlayablePlayerTypeChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+
+
+    public PlayerType GetLocalPlayerType()
+    {
+        return localPlayerType;
+    }
+
+
+    public PlayerType GetCurrentPlayablePlayerType()
+    {
+        return currentPlayablePlayerType;
     }
 
 }
